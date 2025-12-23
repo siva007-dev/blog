f267430895b71b66ab96b92dc51094dbe4324e6d
siva007-dev <siva080204@gmail.com>
2025-12-23 13:40:32 +0530

    added admin

--- blog-project-starter-frontend/src/components/Blogs.jsx
+++ blog-project-starter-frontend/src/components/Blogs.jsx
@@ -20,7 +20,7 @@ function Blogs() {
             if (!user) {
                 navigate("/login");
             } else {
-                setadmim(user.uid === "JgKGjZhnptbIoT26ovCIxGepHSC3");
+                setadmim(user.uid === "Qi1YOVxkHifvcLe27YqSM8hneon1");
             }
             setCheckingAuth(false)
         });
